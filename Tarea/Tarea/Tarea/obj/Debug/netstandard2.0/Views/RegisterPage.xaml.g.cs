8f91d339134ad285b5fcd6bad9cdff270195f1ef
Enmanuelolivo <45018826+Enmanuelolivo@users.noreply.github.com>
2019-08-30 17:55:52 -0400

    Fix

--- Tarea/Tarea/Tarea/obj/Debug/netstandard2.0/Views/RegisterPage.xaml.g.cs
+++ Tarea/Tarea/Tarea/obj/Debug/netstandard2.0/Views/RegisterPage.xaml.g.cs
@@ -16,7 +16,7 @@ namespace Tarea.Views {
     [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views\\RegisterPage.xaml")]
     public partial class RegisterPage : global::Xamarin.Forms.ContentPage {
         
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(RegisterPage));
         }
